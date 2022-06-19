32b54f93360f4660e5271d8bff0aa5db510c97c4
Aaron Gonzalez <a.gonzalez979@gmail.com>
2022-06-19 16:41:00 -0400

    create a dbt project

---
+++ jaffle_shop/target/run/jaffle_shop/models/example/my_second_dbt_model.sql
@@ -0,0 +1,10 @@
+
+
+  create or replace view `agonzalez-dbt-tutorial`.`dbt_agonzalez`.`my_second_dbt_model`
+  OPTIONS()
+  as -- Use the `ref` function to select from other models
+
+select *
+from `agonzalez-dbt-tutorial`.`dbt_agonzalez`.`my_first_dbt_model`
+where id = 1;
+
