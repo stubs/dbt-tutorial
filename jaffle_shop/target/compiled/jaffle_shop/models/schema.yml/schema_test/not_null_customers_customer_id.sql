3fc97821a204705259c2645c96b4d5679ab0f535
Aaron Gonzalez <a.gonzalez979@gmail.com>
2022-06-19 17:40:03 -0400

    Add customers model, tests, docs

---
+++ jaffle_shop/target/compiled/jaffle_shop/models/schema.yml/schema_test/not_null_customers_customer_id.sql
@@ -0,0 +1,9 @@
+
+    
+    
+
+select *
+from `agonzalez-dbt-tutorial`.`dbt_agonzalez`.`customers`
+where customer_id is null
+
+
---
+++ jaffle_shop/target/compiled/jaffle_shop/models/schema.yml/schema_test/not_null_stg_customers_customer_id.sql
@@ -0,0 +1,9 @@
+
+    
+    
+
+select *
+from `agonzalez-dbt-tutorial`.`dbt_agonzalez`.`stg_customers`
+where customer_id is null
+
+
